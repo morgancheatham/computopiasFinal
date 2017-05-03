b9ec9c9ac0c35c9b583a93502d6724cb00577a67
morgancheatham <morgancheatham@gmail.com>
2017-05-02 19:14:16 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter: {
+    consumer_key: '0xRCyZpuM0KWrBlYpW9OgGs3b',
+    consumer_secret: 'LyiBIeONojnGUMYYfjnKPdtxYNCkiMkgAqtALFWeylfX1bkleC',
+    access_token_key: '785805552928509953-voSImWO5isKID4K0xXrbeRDhhf425ky',
+    access_token_secret: 'PuswWu2a9KRHB4L3u6oMrTjhPVRg05RMPDk0zvaF4UGkI'
+  }
+}
